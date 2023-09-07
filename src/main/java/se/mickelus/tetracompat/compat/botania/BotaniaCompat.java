201e07031d537afaaf73a585928d8ba22f07abc3
Mikael E. Wikner <wikner.mikael@gmail.com>
2023-09-07 16:58:22 +0200

    initial commit

---
+++ src/main/java/se/mickelus/tetracompat/compat/botania/BotaniaCompat.java
@@ -0,0 +1,17 @@
+package se.mickelus.tetracompat.compat.botania;
+
+import net.minecraftforge.fml.ModList;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class BotaniaCompat {
+    public static final String modId = "botania";
+    public static final Boolean isLoaded = ModList.get().isLoaded(modId);
+
+    public static void clientInit() {
+        if (isLoaded) {
+            ManaRepair.clientInit();
+        }
+    }
+}
