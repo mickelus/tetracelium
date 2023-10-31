ad55179c069c2ff40fb77744ed49d1f5e492d3af
Mikael E. Wikner <wikner.mikael@gmail.com>
2023-11-01 00:47:20 +0100

    restructured initialization

--- src/main/java/se/mickelus/tetracelium/TetraceliumMod.java
+++ src/main/java/se/mickelus/tetracelium/TetraceliumMod.java
@@ -3,16 +3,18 @@ package se.mickelus.tetracelium;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import se.mickelus.tetra.module.ItemUpgradeRegistry;
-import se.mickelus.tetracelium.compat.apotheosis.AffixReplacementHook;
 import se.mickelus.tetracelium.compat.apotheosis.ApotheosisCompat;
 import se.mickelus.tetracelium.compat.botania.BotaniaCompat;
-import se.mickelus.tetracelium.compat.botania.ManaRepair;
+import se.mickelus.tetracelium.compat.farmersdelight.FarmersDelightCompat;
 import se.mickelus.tetracelium.compat.farmersdelight.provider.FarmersDelightCuttingRecipeProvider;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -25,13 +27,25 @@ public class TetraceliumMod {
     public static final String MOD_ID = "tetracelium";
 
     public TetraceliumMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
+        MinecraftForge.EVENT_BUS.register(this);
+
         TetraceliumRegistries.init(FMLJavaModLoadingContext.get().getModEventBus());
+
+        if (BotaniaCompat.isLoaded) {
+            BotaniaCompat.init();
+        }
+
         if (ApotheosisCompat.isLoaded) {
             ItemUpgradeRegistry.instance.registerReplacementHook(new AffixReplacementHook());
         }
+    }
 
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void clientSetup(FMLClientSetupEvent event) {
         if (BotaniaCompat.isLoaded) {
-            MinecraftForge.EVENT_BUS.register(ManaRepair.class);
+            BotaniaCompat.clientSetup();
         }
     }
 
--- src/main/java/se/mickelus/tetracelium/compat/botania/BotaniaCompat.java
+++ src/main/java/se/mickelus/tetracelium/compat/botania/BotaniaCompat.java
@@ -1,5 +1,6 @@
 package se.mickelus.tetracelium.compat.botania;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModList;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -9,9 +10,11 @@ public class BotaniaCompat {
     public static final String modId = "botania";
     public static final Boolean isLoaded = ModList.get().isLoaded(modId);
 
-    public static void clientInit() {
-        if (isLoaded) {
-            ManaRepair.clientInit();
-        }
+    public static void init() {
+        MinecraftForge.EVENT_BUS.register(ManaRepair.class);
+    }
+
+    public static void clientSetup() {
+        ManaRepair.clientInit();
     }
 }
